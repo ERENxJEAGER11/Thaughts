f4f7f6ca0ddd3c3fa135f0974b5d940239db8136
ERENxJEAGER11 <114978438+ERENxJEAGER11@users.noreply.github.com>
2023-12-11 17:24:55 +0530

    initial commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Thaught from '../src/Components/Thaught/Thaught';
+
+function App() {
+  return (
+    <div className="App">
+      Aman
+      <Thaught/>
+    </div>
+  );
+}
+
+export default App;
